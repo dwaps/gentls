ba6520a51281899eec499b469b7fd3fe3a057da0
Michael CORNILLON <contact@dwaps.fr>
2021-05-19 13:07:07 +0200

    change error log

--- gentls.sh
+++ gentls.sh
@@ -3,7 +3,7 @@
 rm -rfv ssl 2>&1 > /dev/null
 if [ ! -f "ssl" ]; then mkdir ssl; fi
 [[ "${#1}" < 3 ]] && \
-echo "The first argument (name of files) must contain at least 2 letters!" && rmdir ssl && \
+echo "The first argument (name of files) must contain at least 2 letters and less than 10!" && rmdir ssl && \
 exit 1
 touch ssl/.gitkeep
 openssl req -x509 -sha256 -newkey rsa:2048 -days 3650 -nodes -out ssl/$1.crt -keyout ssl/$1.key
